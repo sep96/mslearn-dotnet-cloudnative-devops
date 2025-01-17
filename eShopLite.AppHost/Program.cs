730eac5a73b8064135945d7f22b19fef7800f296
James <sep7696@gmail.com>
2025-01-17 22:04:50 +0330

    Add Postgres and Redis

--- eShopLite.AppHost/Program.cs
+++ eShopLite.AppHost/Program.cs
@@ -1,7 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var db = builder.AddPostgres("db").WithPgAdmin();
 var cache = builder.AddRedis("cache");
-var products = builder.AddProject<Projects.Products>("products");
+var products = builder.AddProject<Projects.Products>("products")
+    .WithReference(db);
 
 builder.AddProject<Projects.Store>("store")
     .WithReference(products)
